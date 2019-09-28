f9d98bdac8ca027305459d0cd2b34a77b880b851
Skjor Bearded ben <sbearded@il-h6.21-school.ru>
2019-09-28 16:44:34 +0300

    add hash init

--- src/21sh/main.c
+++ src/21sh/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yharwyn- <yharwyn-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sbearded <sbearded@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 17:41:11 by yharwyn-          #+#    #+#             */
-/*   Updated: 2019/07/27 18:05:52 by yharwyn-         ###   ########.fr       */
+/*   Updated: 2019/09/28 16:44:06 by sbearded         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,7 @@ void		sh_init(char **environ)
 	while (++i < NR_JOBS)
 		shell->jobs[i] = NULL;
 	built_init();
+	phash_init();
 	sh_update_cwd_info();
 }
 
