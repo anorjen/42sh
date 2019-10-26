ca59ddc9305238c978c1b43338c7e3beca3ae26c
Skjor Bearded ben <sbearded@il-i5.21-school.ru>
2019-10-26 19:40:22 +0300

    fix bugs

--- src/vi_mode/src/input/in_ft_put.c
+++ src/vi_mode/src/input/in_ft_put.c
@@ -3,17 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   in_ft_put.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-<<<<<<< HEAD:src/vi_mode/src/input/in_ft_put.c
 /*   By: sbearded <sbearded@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 15:11:39 by anorjen           #+#    #+#             */
-/*   Updated: 2019/10/26 17:03:06 by sbearded         ###   ########.fr       */
-=======
-/*   By: anorjen <anorjen@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/23 15:11:39 by anorjen           #+#    #+#             */
-/*   Updated: 2019/09/28 15:34:07 by anorjen          ###   ########.fr       */
->>>>>>> anorjen:src/vi_mode/src/input/in_ft_put.c
+/*   Updated: 2019/10/26 19:14:39 by sbearded         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
